6e04de879dfdb287701e4b03a8bca056e8b9872f
unknown <24abdullahalmasum@gmail.com>
2023-05-01 20:08:45 +0600

    added cooomand

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const catagories = require('./data/catagories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -14,7 +15,29 @@ app.get('/catagories', (req, res) => {
     res.send(catagories)
 })
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
 
-app.listen(port,()=>{
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/catagories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews);
+    }
+})
+
+
+app.listen(port, () => {
     console.log(`dragon is listening on port${port}`)
 })
